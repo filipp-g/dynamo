50c76d59d1f5b33cfabe5e828754ec0c082b8e04
Filipp G <8738425+filipp-g@users.noreply.github.com>
2020-01-18 17:34:03 -0500

    xml GET request working

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+# Import modules
 from flask import Flask
- 
+import xmltodict
+import urllib3
+
 app = Flask(__name__)
-  
+
+url = 'http://reports.ieso.ca/public/RealtimeConstTotals/PUB_RealtimeConstTotals.xml'
+
+http = urllib3.PoolManager()
+response = http.request('GET', url)
+
+data = xmltodict.parse(response.data)
+
 @app.route('/')
 def index():
-    return("Hello World!")
+    return(data)
