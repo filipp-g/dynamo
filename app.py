ae8e7c0b0da060f07eecbb51464639f96d3ae2f4
Filipp G <8738425+filipp-g@users.noreply.github.com>
2020-01-18 19:08:35 -0500

    get Ontario total demand

--- app.py
+++ app.py
@@ -9,9 +9,16 @@ url = 'http://reports.ieso.ca/public/RealtimeConstTotals/PUB_RealtimeConstTotals
 
 http = urllib3.PoolManager()
 response = http.request('GET', url)
-
 data = xmltodict.parse(response.data)
 
 @app.route('/')
 def index():
-    return(data)
+    intervals = data.get("Document").get("DocBody").get("Energies").get("IntervalEnergy")
+    intervals_len = str(len(intervals))
+    for item in intervals:
+        if item.get("Interval") == intervals_len:
+            market_quant = item.get("MQ")
+            for mq in market_quant:
+                if mq.get("MarketQuantity") == "ONTARIO DEMAND":
+                    return mq.get("EnergyMW")
+    return("Not found")
